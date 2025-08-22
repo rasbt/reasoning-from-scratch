04ebe450e54fa65df5ea8a614be99a585114c718
rasbt <mail@sebastianraschka.com>
2025-08-21 19:55:21 -0500

    formatting

--- reasoning_from_scratch/qwen3.py
+++ reasoning_from_scratch/qwen3.py
@@ -15,17 +15,17 @@ import torch.nn as nn
 
 # 0.6 billion parameters
 QWEN_CONFIG_06_B = {
-    "vocab_size": 151_936,           # Vocabulary size
-    "context_length": 40_960,        # Context length that was used to train the model
-    "emb_dim": 1024,                 # Embedding dimension
-    "n_heads": 16,                   # Number of attention heads
-    "n_layers": 28,                  # Number of layers
-    "hidden_dim": 3072,              # Size of the intermediate dimension in FeedForward
-    "head_dim": 128,                 # Size of the heads in GQA
-    "qk_norm": True,                 # Whether to normalize queries and keys in GQA
-    "n_kv_groups": 8,                # Key-Value groups for grouped-query attention
-    "rope_base": 1_000_000.0,        # The base in RoPE's "theta"
-    "dtype": torch.bfloat16,         # Lower-precision dtype to reduce memory usage
+    "vocab_size": 151_936,     # Vocabulary size
+    "context_length": 40_960,  # Length originally used during training
+    "emb_dim": 1024,           # Embedding dimension
+    "n_heads": 16,             # Number of attention heads
+    "n_layers": 28,            # Number of layers
+    "hidden_dim": 3072,        # Size of intermediate dim in FeedForward
+    "head_dim": 128,           # Size of the heads in GQA
+    "qk_norm": True,           # Whether to normalize queries & keys in GQA
+    "n_kv_groups": 8,          # Key-Value groups for GQA
+    "rope_base": 1_000_000.0,  # The base in RoPE's "theta"
+    "dtype": torch.bfloat16,   # Lower-precision dtype to reduce memory
 }
 
 
