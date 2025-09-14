acd41754ef34d565ef6f6faaa47493322ba17875
rasbt <mail@sebastianraschka.com>
2025-09-14 12:28:53 -0500

    Update compile for PyTorch 2.8

--- ch02/03_optimized-LLM/compare_inference.py
+++ ch02/03_optimized-LLM/compare_inference.py
@@ -101,6 +101,12 @@ model.load_state_dict(torch.load(model_file, map_location=device))
 model.to(device)
 
 if args.compile:
+    major, minor = map(int, torch.__version__.split(".")[:2])
+    if (major, minor) >= (2, 8):
+        # This avoids retriggering model recompilations
+        # in PyTorch 2.8 and newer
+        # if the model contains code like self.pos = self.pos + 1
+        torch._dynamo.config.allow_unspec_int_on_nn_module = True
     model = torch.compile(model)
 
 #########################
