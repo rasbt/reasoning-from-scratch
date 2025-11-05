dd6adac5d19c83430927adb8a557ff4e158129fb
rasbt <mail@sebastianraschka.com>
2025-11-04 20:35:04 -0600

    Swap fc2 and fc3 names in GLU

--- reasoning_from_scratch/qwen3.py
+++ reasoning_from_scratch/qwen3.py
@@ -135,14 +135,14 @@ class FeedForward(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.fc1 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
-        self.fc2 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
-        self.fc3 = nn.Linear(cfg["hidden_dim"], cfg["emb_dim"], dtype=cfg["dtype"], bias=False)
+        self.fc2 = nn.Linear(cfg["hidden_dim"], cfg["emb_dim"], dtype=cfg["dtype"], bias=False)
+        self.fc3 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
 
     def forward(self, x):
         x_fc1 = self.fc1(x)
-        x_fc2 = self.fc2(x)
-        x = nn.functional.silu(x_fc1) * x_fc2
-        return self.fc3(x)
+        x_fc3 = self.fc3(x)
+        x = nn.functional.silu(x_fc1) * x_fc3
+        return self.fc2(x)
 
 
 class GroupedQueryAttention(nn.Module):
